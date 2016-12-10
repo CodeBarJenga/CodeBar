94ba74399466de7d89de83b4942d16d90e2e092b
nitinpamnani002@gmail.com <nitinpamnani002@gmail.com>
2016-12-11 02:13:48 +0530

    Structure of team page

---
+++ view/OurTeam.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!--<meta charset="utf-8">
+
+		<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
+		Remove this if you use the .htaccess
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+		<meta name="description" content="">
+		<meta name="author" content="LENOVO">-->
+		
+		<title>CodeBar Team</title>
+		<?php
+		include 'depend.php';
+		?>
+	</head>
+
+	<body >
+		<div  class="container-fluid">
+     		<div class="row"  >
+				<?php
+				include 'header.php';
+				?>
+			</div>
+
+
+			<div class="row">
+				<div class="container-fluid">
+					<?php
+					include 'footer.php';
+					?>
+				</div>
+			</div>
+
+		</div>
+		<!--fluid container div ends-->
+	</body>
+</html>
